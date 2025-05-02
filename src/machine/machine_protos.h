2d001e103e3adc0c63412ed9df0240aae1902010
Ronny Hansen <ronnyha@gmail.com>
2025-05-02 14:05:18 +0200

    WASM is working

--- src/machine/machine_protos.h
+++ src/machine/machine_protos.h
@@ -1,18 +1,6 @@
 /* AUTO-GENERATED FILE. DO NOT EDIT! */
-/* Prototypes from io.c */
 
-/* io.c */
-void IO_Init(void);
-void IO_Destroy(void);
-uint16_t IO_Read(uint32_t address);
-void IO_Write(uint32_t address, uint16_t value);
-int IO_Ident(uint16_t level);
-void IO_Tick(void);
-ushort io_op(ushort ioadd, ushort regA);
-
-/* Prototypes from machine.c */
-
-/* machine.c */
+/* /home/ronny/repos/nd100x/src/machine/machine.c */
 void machine_init(bool debuggerEnabled);
 void cleanup_machine(void);
 void machine_run(int ticks);
@@ -20,3 +8,11 @@ void machine_stop(void);
 void setdefaultconfig(void);
 void program_load(BOOT_TYPE bootType, char *imageFile, bool verbose);
 
+/* /home/ronny/repos/nd100x/src/machine/io.c */
+void IO_Init(void);
+void IO_Destroy(void);
+uint16_t IO_Read(uint32_t address);
+void IO_Write(uint32_t address, uint16_t value);
+int IO_Ident(uint16_t level);
+void IO_Tick(void);
+ushort io_op(ushort ioadd, ushort regA);
