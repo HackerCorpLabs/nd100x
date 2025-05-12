f5459fb7a47356a75f08d82ce1bbfa3d3b2b54ce
Ronny Hansen <ronnyha@gmail.com>
2025-05-12 11:30:53 +0200

    Enhance Debugger Functionality with Stack Trace and Step Types
    - Introduced a stack trace structure to maintain and track stack frames during debugging.
    - Added step types for the debugger to support "next", "step in", and "step out" commands, improving single-stepping capabilities.
    - Updated signal handling to allow for graceful termination of the debugger thread.
    - Refactored the debugger thread management to include periodic checks for exit conditions.
    
    These enhancements improve the debugging experience by providing better control over execution flow and stack management.

--- src/debugger/debugger.h
+++ src/debugger/debugger.h
@@ -1,6 +1,8 @@
 #ifndef _DEBUGGER_H_
 #define _DEBUGGER_H_
 
+#include <stdint.h>
+
 #ifdef _WIN32
     #define THREAD_FUNC DWORD WINAPI
     
@@ -12,6 +14,36 @@
     #define THREAD_RETURN(val) return (void *)(val)
     #define WIN_RESULT void*
 #endif
+
+// Define the maximum number of stack frames we'll track
+#define MAX_STACK_FRAMES 20
+
+    // Static variables to maintain stack frame state
+typedef struct {
+    uint16_t pc;
+    uint16_t operand;
+    uint16_t return_address;
+} StackFrame;
+
+typedef struct {
+    StackFrame frames[MAX_STACK_FRAMES];
+    int current_frame;
+    int frame_count;
+} StackTrace;
+
+
+/// @brief Step types for the debugger. Maps to DAP
+typedef enum  {
+
+    /// @brief DAP "next" command
+    STEP_OVER,
+
+    /// @brief DAP "stepIn" command
+    STEP_IN,
+
+    /// @brief DAP "stepOut" command
+    STEP_OUT,
+} StepType;
 
 // Function declarations
 void start_debugger();
